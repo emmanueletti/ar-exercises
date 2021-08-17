d138c0b91076b5bad538f141b5ccff117499fe8b
Emmanuel Etti <emmanueletti@gmail.com>
2021-08-17 03:52:12 +0000

    completes exercise 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,31 @@
 require_relative '../setup'
+# require 'active_record'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
 # Your code goes below here ...
+# create the "Store" model automagically linked to the database table "stores" via activerecord
+# database created in setup via activerecord
+class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 2 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
+  validates_associated :store
+end
+
+# use the class model to query the database with active connection supplied via setup
+Store.create(name: "Burnaby", annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
+
+Store.create(name: "Richmond", annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true)
+
+Store.create(name: "Gastown", annual_revenue: 190_000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count
+
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,21 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+# p Employee.create!(first_name: "test", last_name: "mctest", hourly_rate: 50)
+
+puts "Type in a store name: "
+store_name = gets.chomp
+
+@new_store = Store.create(name: store_name)
+p @new_store.errors.full_messages.each { |message| puts "Error: #{message}" }
